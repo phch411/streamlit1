8d23fb9f9d055f9eedc5854e66afb1b26631caa7
phch411 <phch411@gmail.com>
2024-04-20 23:11:33 +0900

    add: app.py input 추가

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
-st.title("Hello, 박쌤!")
+st.title("Hello, 박쌤!🎈")
+name = st.text_input("이름을 입력해주세요.")
+if name != "":
+    st.write(f"{name}님! 저의 페이지에 오신 것을 환영합니다.")
+
+st.balloons()
